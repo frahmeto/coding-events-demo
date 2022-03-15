ee437fb7090e1cf8e834cf5c440dbe357e3b8a1e
Fidel Rahmeto <fidyod@gmail.com>
2022-03-14 19:41:48 -0500

    done

--- src/main/java/org/launchcode/codingevents/controllers/EventController.java
+++ src/main/java/org/launchcode/codingevents/controllers/EventController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
 /**
@@ -17,13 +18,20 @@ import java.util.List;
 @RequestMapping("events")
 public class EventController {
 
-    private static List<String> events = new ArrayList<>();
+   // private static List<String> events = new ArrayList<>();
 
     @GetMapping
     public String displayAllEvents(Model model) {
-        model.addAttribute("title", "All Events");
-        model.addAttribute("events", events);
-        return "events/index";
+//        model.addAttribute("title", "All Events");
+//       model.addAttribute("events", events);
+//       return "events/index";
+      HashMap<String, String> events = new HashMap<>();
+
+      events.put("Pie Pizza Eating Contest", "Winner wins lifetime supply of ranch!");
+      events.put("Implementation Instrumentation","Can ypu code using a piano?, let's find out.." );
+      events.put("Silicon Styles", "A Fashion event for WFH programmers");
+      model.addAttribute("events", events);
+      return "events/index";
     }
 
     @GetMapping("create")
@@ -34,7 +42,7 @@ public class EventController {
 
     @PostMapping("create")
     public String processCreateEventForm(@RequestParam String eventName) {
-        events.add(eventName);
+        //events.add(eventName);
         return "redirect:";
     }
 
